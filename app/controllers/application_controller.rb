d7e63504e0fb14a64e787ead28e1416340a69b4f
Liptonite24 <alex.chuvand@gmail.com>
2016-10-18 17:47:14 +1100

    fix omniauth facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,14 +15,17 @@ class ApplicationController < ActionController::Base
 
   #redirect after signin
   def after_sign_in_path_for(resource_or_scope)
-    if (resource.has_role? :student) && (resource.student_profile.nil?)
+    if !resource.provider.nil? && resource.roles.empty?
+      flash[:success]= "Choose a Role"
+      edit_user_registration_path(resource)
+    elsif (resource.has_role? :student) && (resource.student_profile.nil?)
       flash[:success]= "Create a Student profile to get started."
-       new_student_profile_path
+      new_student_profile_path
     elsif (resource.has_role? :tutor) && (resource.tutor_profile.nil?)
       flash[:success]= "Create a Tutor profile to get started."
-       new_tutor_profile_path
+      new_tutor_profile_path
     else
-       root_path
+      root_path
     end
   end
 
